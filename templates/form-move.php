ba8c8c877ef26d1acab39a4193b05b0d192b6e08
Joseph Gabito <codehaiku.io@gmail.com>
2017-03-04 14:15:28 +0800

    added move form

---
+++ templates/form-move.php
@@ -0,0 +1,20 @@
+<h3>Move File</h3>
+<form method="POST" action="">
+	<div class="input-group">
+		<label for="sdCopyDirectory">Select a directory</label>
+		<select v-model="moveActionSelectedDir" name="directory" id="sdCopyDirectory">
+			<option v-for="directory in directories">{{directory.name}}</option>
+		</select>
+	</div>
+	<div class="stageFilesList" v-if="stageFiles.length!=0">
+		The following files will be move to {{moveActionSelectedDir}}:
+		<ul class="stageFilesListUl">
+			<li class="stageFilesListItem" v-for="stageFile in stageFiles">
+				{{stageFile.name}}
+			</li>
+		</ul>
+	</div>
+	<div class="input-group">
+		<input v-on:click.prevent="" type="submit" value="Move File" />
+	</div>
+</form>
