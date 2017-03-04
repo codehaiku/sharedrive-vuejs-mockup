ee6018e0a49305a726488817be13c3a455616ac6
Joseph Gabito <codehaiku.io@gmail.com>
2017-03-04 14:15:28 +0800

    added rename form

---
+++ templates/form-rename.php
@@ -0,0 +1,10 @@
+<h3>Rename File</h3>
+<form method="POST" action="">
+	<div class="input-group">
+		<label for="sdRenameField">Enter a new name:</label>
+		<input type="text" value=""  />
+	</div>
+	<div class="input-group">
+		<input v-on:click.prevent="" type="submit" value="Rename File" />
+	</div>
+</form>
